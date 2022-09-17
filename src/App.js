16b2348b41ba6a5db2d74631a1297ef8ca78a19b
jcaicedo <jose.caicedo@outlook.com>
2022-09-16 21:54:21 -0500

    feat(Logica de creacion de tarea)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import Task from "./components/task/task";
+import TaskForm from "./components/taskForm/taskForm";
+import {useState} from 'react'
 
 function App() {
+
+  const [tasks, setTasks] = useState([])
+
+  const addTask = task =>{
+    if(task.text.trim()){
+      task.text = task.text.trim()
+      const updatedTask = [task, ...tasks]
+      setTasks(updatedTask)
+    }
+  }
+
+  const deleteTask = id =>{
+    const updatedTask = tasks.filter( task => task.id !== id)
+    setTasks(updatedTask)
+  }
+
+  const completedTask = id =>{
+    const updatedTask = tasks.map( task => {
+      if (task.id === id){
+        task.completed = !task.completed
+      }
+      return task
+    })
+    setTasks(updatedTask)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <div className="App">
+        <h1 className='title'>Todo list</h1>
+      </div>
+      <div className="list_task">
+        <h3>Mis tareas</h3>
+        <TaskForm onSubmit={addTask} />
+        {
+          tasks.map((task) =>
+            <Task
+            key={task.id}
+            id={task.id}
+            text={task.text}
+            completed={task.completed}
+            deleteTask={deleteTask}
+            completedTask={completedTask} />
+          )
+        }
+      </div>
     </div>
+    </>
   );
 }
 
